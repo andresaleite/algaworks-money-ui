1c2406ceae622860df7b93c3fa3d5cea5d8fa3d5
Andresa <andresaleite@gmail.com>
2018-11-15 19:38:24 -0200

    aula feita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+  nome: String;
+  email: String;
+  profissao: String;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  cliente = new Cliente;
   profissoes = ['Programador', 'Empresario', 'Outro'];
   profissaoIncial = 'Outro';
   salvar(form: NgForm) {
-    console.log('salvando');
-    console.log(this.profissaoIncial);
-    console.log(form.value.profissao);
+   this.cliente.nome = form.value.primeiroNome;
+   //this.cliente.email = form.value.email;
+   this.cliente.profissao = form.value.profissao;
+
+   console.log(form.value);
+   console.log(this.cliente);
   }
 }
